6eab5b5816d257fe496c50e9e2c4864f0040eea8
SonPham <phamquangson1994@gmail.com>
2023-10-05 06:39:03 +0700

    Support Internal Identity url

--- src/Play.Common/Settings/ServiceSettings.cs
+++ src/Play.Common/Settings/ServiceSettings.cs
@@ -5,6 +5,9 @@ public class ServiceSettings
     public string ServiceName { get; init; }
 
     public string Authority { get; init; }
+    public string InternalHostAuthority { get; init; } = string.Empty;
+
+    public string InternalAuthority => $"http://{InternalHostAuthority}";
 
     public string MessageBroker { get; init; }
     public string KeyVaultName { get; init; }

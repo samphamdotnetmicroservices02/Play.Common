6baea59daecd6c66f86972a657a05ad44be0a920
SonPham <phamquangson1994@gmail.com>
2023-10-16 18:03:17 +0700

    Configure AzureKeyvault for Kubernetes local

--- src/Play.Common/Configurations/Extensions.cs
+++ src/Play.Common/Configurations/Extensions.cs
@@ -16,23 +16,24 @@ public static class Extensions
                     {
                         var configuration = configurationBuilder.Build();
                         var serviceSetting = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-
-                        configurationBuilder.AddAzureKeyVault(
-                            //Uri is from your key vault -> Vault URI
-                            new Uri($"https://{serviceSetting.KeyVaultName}.vault.azure.net/"),
-                            /*
-                            * The idead of DefaultAzureCredential is that the identity Nuget package we just added will try to figure out what is the best way
-                            * to accquire credentials or to use credentials for connecting to this key vault. So it will try a bunch of things depending on
-                            * what's available in the current environment. Now in our case, what we want is for the microservice process to use the current
-                            * identity that has been assigned to the port where our microservice is running. And this is where the Azure AD port managed 
-                            * identities feature comes into place. Because we need to assign an identity that has access to the key vault so that when the
-                            * microservies runs in a port in Kubernetes, it will have right away access to connect to the key vault. We'll talk more about 
-                            * Azure AD port managed identities in the next lesson, but for now let's just create a brand new docker image that we will have
-                            * now support for the key vault configuration service.
-                            */
-                            new DefaultAzureCredential()
-                        );
+                        if (!serviceSetting.IsKubernetesLocal.Equals("true", StringComparison.OrdinalIgnoreCase))
+                        {
+                            configurationBuilder.AddAzureKeyVault(
+                                //Uri is from your key vault -> Vault URI
+                                new Uri($"https://{serviceSetting.KeyVaultName}.vault.azure.net/"),
+                                /*
+                                * The idead of DefaultAzureCredential is that the identity Nuget package we just added will try to figure out what is the best way
+                                * to accquire credentials or to use credentials for connecting to this key vault. So it will try a bunch of things depending on
+                                * what's available in the current environment. Now in our case, what we want is for the microservice process to use the current
+                                * identity that has been assigned to the port where our microservice is running. And this is where the Azure AD port managed 
+                                * identities feature comes into place. Because we need to assign an identity that has access to the key vault so that when the
+                                * microservies runs in a port in Kubernetes, it will have right away access to connect to the key vault. We'll talk more about 
+                                * Azure AD port managed identities in the next lesson, but for now let's just create a brand new docker image that we will have
+                                * now support for the key vault configuration service.
+                                */
+                                new DefaultAzureCredential()
+                            );
+                        }
                     }
                 });
     }
